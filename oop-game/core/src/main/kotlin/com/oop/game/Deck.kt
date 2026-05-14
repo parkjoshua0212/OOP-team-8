dce2f91c77b5a542855cefad745828fc750cc5fe
jaeyoung <parkjoshua0212@gmail.com>
2026-05-14 15:21:52 +0900

    simple Deck.kt fix

--- oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
+++ oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
@@ -1,4 +1,3 @@
-
 class Deck {
     val suit = listOf("Hearts", "Diamonds", "Clubs", "Spades")
     val rank = listOf("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
@@ -15,26 +14,6 @@ class Deck {
 
     fun shuffledDeck(): List<String> {
         return createDeck().shuffled()
-    }
-}
-
-class CardValue {
-    val cardValues = mapOf{
-        "2" to 2,
-        "3" to 3,
-        "4" to 4,
-        "5" to 5,
-        "6" to 6,
-        "7" to 7,
-        "8" to 8,
-        "9" to 9,
-        "10" to 10,
-        "Jack" to 10,
-        "Queen" to 10,
-        "King" to 10,
-        // Ace can be 1 or 11 
-        //if Value = Ace + 10 <= 21 then Ace = 11 
-        //else Ace = 1
     }
 }
 
