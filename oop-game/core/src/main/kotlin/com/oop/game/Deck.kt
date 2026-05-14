71615358abd1cbb2248bee68238311fa53302b8e
jaeyoung <parkjoshua0212@gmail.com>
2026-05-14 14:21:50 +0900

    Added Deck.kt code -> shuffles cards

---
+++ oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
@@ -0,0 +1,24 @@
+
+class Deck {
+    val suit = listOf("Hearts", "Diamonds", "Clubs", "Spades")
+    val rank = listOf("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
+
+    fun createDeck(): List<String> {
+        val deck = mutableListOf<String>()
+        for (s in suit) {
+            for (r in rank) {
+                deck.add("$r of $s")
+            }
+        }
+        return deck
+    }
+
+    fun shuffledDeck(): List<String> {
+        return createDeck().shuffled()
+    }
+}
+
+fun main() {
+    val deck = Deck()
+    println(deck.shuffledDeck())
+}
