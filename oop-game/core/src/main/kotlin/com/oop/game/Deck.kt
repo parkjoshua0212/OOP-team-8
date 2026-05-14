c0e67342907d2208cf3a84380436328737252607
jaeyoung <parkjoshua0212@gmail.com>
2026-05-14 14:27:29 +0900

    Deck cardvalues, incomplete

--- oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
+++ oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
@@ -18,6 +18,34 @@ class Deck {
     }
 }
 
+class DealerValue{
+
+}
+
+class UserValue{
+
+}
+
+class CardValue {
+    val cardValues = mapOf{
+        "2" to 2,
+        "3" to 3,
+        "4" to 4,
+        "5" to 5,
+        "6" to 6,
+        "7" to 7,
+        "8" to 8,
+        "9" to 9,
+        "10" to 10,
+        "Jack" to 10,
+        "Queen" to 10,
+        "King" to 10,
+        // Ace can be 1 or 11 
+        //if Value = Ace + 10 <= 21 then Ace = 11 
+        //else Ace = 1
+    }
+}
+
 fun main() {
     val deck = Deck()
     println(deck.shuffledDeck())
