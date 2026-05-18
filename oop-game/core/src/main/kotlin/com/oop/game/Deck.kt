55f3c289222b835429866fb80744656fba41866e
heok_2 <fander22@naver.com>
2026-05-18 20:48:22 +0900

    Deck, Player: Card 클래스 추가, String 방식 수정

--- oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
+++ oop-game/core/src/main/kotlin/com/oop/game/Deck.kt
@@ -1,4 +1,6 @@
 package com.oop.game
+
+//카드 무늬 - 카드 무늬는 4개의 선택지 고정이므로 enum 클래스로
 enum class Suit(val symbol: String, val displayName: String) {
     HEARTS  ("♥", "Hearts"),
     DIAMONDS("♦", "Diamonds"),
@@ -6,6 +8,7 @@ enum class Suit(val symbol: String, val displayName: String) {
     SPADES  ("♠", "Spades")
 }
 
+//카드 숫자 - 위와 마찬가지
 enum class Rank(val displayName: String, val value: Int) {
     TWO  ("2", 2),
     THREE("3", 3),
@@ -16,31 +19,43 @@ enum class Rank(val displayName: String, val value: Int) {
     EIGHT("8", 8),
     NINE ("9", 9),
     TEN  ("10", 10),
-    JACK ("Jack", 10),
-    QUEEN("Queen", 10),
-    KING ("King", 10),
-    ACE  ("Ace", 11);   // Ace 1/11 처리는 Hand 단계에서
+    JACK ("J", 10),
+    QUEEN("Q", 10),
+    KING ("K", 10),
+    ACE  ("A", 11);
+    // 블랙잭에서 Ace는 1 또는 11 둘 다 가능
+    // Ace 1/11 처리는 Hand 단계에서
 
     override fun toString(): String = displayName
 }
 
-class Deck {
-    private val cards = mutableListOf<String>()
+//카드 클래스 생성 - 출력 시 "K♤"와 같은 형태로 출력
+class Card(val suit : Suit, val rank : Rank) {
+    override fun toString(): String = rank.displayName + suit.symbol
+}
 
+class Deck {
+    //private으로 지정하여 밖에서는 못 보도록 설계
+    private val cards = mutableListOf<Card>()
+
+    //리스트에 카드 52장을 무작위로 섞어 넣음
     init {
         for (s in Suit.values()) {
             for (r in Rank.values()) {
-                cards.add("$r of ${s.displayName}")
+                cards.add(Card(s, r))
             }
         }
         cards.shuffle()
     }
 
+    //남은 카드 갯수 세기
     val size: Int get() = cards.size
 
-    fun dealCard(): String {
+    //리스트 맨 뒤 카드를 꺼내서 반환
+    fun dealCard(): Card {
         return cards.removeAt(cards.size - 1)
     }
 
+    //덱이 비었는지 확인
     fun isEmpty(): Boolean = cards.isEmpty()
 }
--- oop-game/core/src/main/kotlin/com/oop/game/Player.kt
+++ oop-game/core/src/main/kotlin/com/oop/game/Player.kt
@@ -1,70 +1,48 @@
 package com.oop.game
 
-class Score() { //Sc0re class into a different property, need to fix later
-        val cardValues = mapOf(
-            "2" to 2,
-            "3" to 3,
-            "4" to 4,
-            "5" to 5,
-            "6" to 6,
-            "7" to 7,
-            "8" to 8,
-            "9" to 9,
-            "10" to 10,
-            "Jack" to 10,
-            "Queen" to 10,
-            "King" to 10,
-            "Ace" to 11 //Ace need to be 1 or 11 depedning on players score, fix later
-        )
-
-        fun calculateScore(hand: MutableList<String>): Int {
-            var score = 0
-            var aceCount = 0
-            for (card in hand) {
-                val rank = card.split(" ")[0]
-                score += cardValues[rank] ?: 0
-                if (rank =="Ace") {
-                    aceCount++
-                }
-            }
-            // Adjust for aces if the score is over 21
-            while (score > 21 && aceCount > 0) {
-                score -= 10
-                aceCount--
-            }
-            return score
-        }
+fun calculateScore(hand: MutableList<Card>): Int {
+    var score = 0
+    var aceCount = 0
+    for (card in hand) {
+        val rank = card.rank.value
+        score += rank
+        if (card.rank == Rank.ACE)
+            aceCount++
+    }
+    // 점수가 21점 이상일 경우 에이스 조정
+    while (score > 21 && aceCount > 0) {
+        score -= 10
+        aceCount--
+    }
+    return score
 }
 
-class Player{ //Score class is for calculating player's score, need to fix later parent class and how to use it in player and dealer class
+class Player { //Score class is for calculating player's score, need to fix later parent class and how to use it in player and dealer class
 //Score shouldn't be a parent class of player and dealer, need to fix later, maybe create a method in score class to calculate score and call it in player and dealer class
-    var hand = mutableListOf<String>()
+    var hand = mutableListOf<Card>()
     var playerScore = 0
-    val score = Score()
 
     fun drawCard(deck: Deck) {
         if (!deck.isEmpty()) {
             val card = deck.dealCard()
             hand.add(card)
-            playerScore = score.calculateScore(hand)
+            playerScore = calculateScore(hand)
         }
-    }
     }
     //Need to create player draw 2 cards at beginning of turn and then decide to draw more or not
     //Beginning of turn -> Player draw card, dealer draw, player draw, dealer draw.//check if this is the right way to call startTurn() method from GameTurn class
     //One card of the dealer is hidden
-
+}
 
 class Dealer {//same with dealer class, fix later
-    var hand = mutableListOf<String>()
+    var hand = mutableListOf<Card>()
     var dealerScore = 0
-    val score = Score()
 
     fun drawCard(deck: Deck) {
         if (!deck.isEmpty()) {
             val card = deck.dealCard()
             hand.add(card)
-            dealerScore = score.calculateScore(hand)
+            dealerScore = calculateScore(hand)
         }
     }
     
